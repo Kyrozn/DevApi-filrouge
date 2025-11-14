59159804bdad726d4739d52646c554c2991e5563
dkyrian <kyrian.delaplace@ynov.com>
2025-11-14 13:51:21 +0100

    index.ts + setup jest mock db

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
 import authRoutes from "./routes/auth.routes";
 import userRoutes from "./routes/user.routes";
-import AdminRoutes from "./routes/admin.routes"
+import AdminRoutes from "./routes/admin.routes";
+import swaggerSpec from "./config/swagger"
+import EventRoutes from "./routes/event.routes"
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// 🧭 Route Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/admin", AdminRoutes);
+app.use("/event", EventRoutes);
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
-);
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () =>
+    console.log(`🚀 Server running on http://localhost:${PORT}`)
+  );
+}
+
+export default app;
